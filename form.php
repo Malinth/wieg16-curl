efe7e7ca5b207eea9dcabaeddd45b02c8e6e78d8
Malinth <malin.thunberg@hotmail.com>
2017-11-11 12:15:11 +0100

    Form update

--- form.php
+++ form.php
@@ -21,45 +21,26 @@ $pdo = new PDO($dsn, $user, $password, $options);
 ?>
 
 				
-<h1>Lägg till en artist</h1>
+<h1>Add a new artist here</h1>
 <form action="form.php" method="post">
-<input type="text" name="fdjurnamn" placeholder="Djurnamn" value="" /> <br>
-<input type="text" name="fantalBen" placeholder="Antal ben" value="" /> <br>
-<input type="text" name="ftyp" placeholder="Djurtyp" value="" /> <br>
-
-<button type="submit">Lägg till nytt djur</button>
+<input type="text" name="name" placeholder="Name" value="" /> <br>
+<input type="text" name="song" placeholder="Song" value="" /> <br>
+<button type="submit">Add a new artist</button>
 </form>
 
 <?php
-if( isset($_POST['fdjurnamn']) ) {
-// kolla om djuret finns i databasen
-// jämföra kolumnen `namn` med $_POST['fdjurnamn']
-$sql = "SELECT COUNT(*) AS 'antal_rader' FROM `djur` WHERE namn = :djurnamn";
-$stm_count = $pdo->prepare($sql);
-$stm_count->execute(['djurnamn' => $_POST['fdjurnamn']]);
-foreach( $stm_count as $row ) {
-$antal_rader = $row['antal_rader'];
-}
+if( isset($_POST['name']) ) {
 
-if( $antal_rader > 0 ) {
-// uppdatera befintligt djur
-$sql = "UPDATE `djur` SET `antalBen` = :vantalben, `typ` = :vtyp WHERE `namn` = :vdjurnamn";
-$stm_update = $pdo->prepare($sql);
-$stm_update->execute(['vantalben' => $_POST['fantalBen'], 'vtyp' => $_POST['ftyp'], 'vdjurnamn' => $_POST['fdjurnamn']]);
-echo "<p>{$_POST['fdjurnamn']} fanns redan, så nu är den uppdaterade.</p>";
-
-} else {
 // lägg till nytt
-$sql = "INSERT INTO `djur` (`namn`, `antalBen`, `typ`) VALUES (:djurnamn, :antalben, :djurtyp) ";
+$sql = "INSERT INTO `artists` (`name`, `song`) VALUES (:name, :song) ";
 $stm_insert = $pdo->prepare($sql);
-$stm_insert->execute(['djurnamn' => $_POST['fdjurnamn'], 'antalben' => $_POST['fantalBen'], 'djurtyp' => $_POST['ftyp']]);
-echo "<p>Du lade till nytt djur !!</p>";
+$stm_insert->execute(['name' => $_POST['name'], 'song' => $_POST['song']]);
+echo "<p>You added a new artist!</p>";
+
 }
 
-} else {
-echo "<p>Lägg gärna till en artist i tabellen.</p>";
-}
 ?>
+
 
 </body>
 </html>
