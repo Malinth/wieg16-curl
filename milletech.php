aa0a92cd754e92853f0528706ddf6a7ed0af9ec7
Malinth <malin.thunberg@hotmail.com>
2017-11-11 14:36:22 +0100

    Uppdatering

---
+++ milletech.php
@@ -0,0 +1,41 @@
+<?php
+
+/* Uppgift 3 Analysera datan, skapa tabeller utifrån informationen som ska hämtas
+
+[
+    {
+        "email": "marcus.dalgren@overlay.se",
+        "firstname": "Marcus",
+        "lastname": "DalDalgren",
+        "gender": "1",
+        "customer_activated": "1",
+        "group_id": "4",
+        "customer_company": "Overlay AB",
+        "default_billing": "1",
+        "default_shipping": "1",
+        "is_active": "1",
+        "created_at": "2011-03-31 12:38:00",
+        "updated_at": "2016-11-23 10:56:07",
+        "customer_invoice_email": null,
+        "customer_extra_text": null,
+        "customer_due_date_period": null,
+        "id": "1",
+        
+
+        "address": {
+            "id": "1",
+            "customer_id": "1",
+            "customer_address_id": null,
+            "email": null,
+            "firstname": "Marcus",
+            "lastname": "Dalgren",
+            "postcode": "43644",
+            "street": "Askims mabäcksväg 10",
+            "city": "Göteborg",
+            "telephone": "0736964744",
+            "country_id": "SE",
+            "address_type": null,
+            "company": "Overlay AB",
+            "country": "Sverige"
+        }
+    },*/
